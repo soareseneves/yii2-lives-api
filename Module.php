317a6af1a7edb31e71f6cb0e546be042c18a5613
Douglas Soares <douglas.srs12@gmail.com>
2020-12-08 09:49:37 -0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -149,7 +149,7 @@ class Module extends \yii\base\Module
             $this->googleYoutubeLiveBroadcast->setSnippet($this->googleLiveBroadcastSnippet);
             $this->googleYoutubeLiveBroadcast->setStatus($this->googleLiveBroadcastStatus);
             $this->googleYoutubeLiveBroadcast->setKind($data['kind']);
-            //$this->googleYoutubeLiveBroadcast->setMadeForKids(false);
+            $this->googleYoutubeLiveBroadcast->setMadeForKids(false);
 
             /**
              * Execute Insert LiveBroadcast Resource Api [return an object that contains information about the new broadcast]
